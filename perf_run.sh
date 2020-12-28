b21fef7a411f926e2e69531eb9a68099e17138ab
EP2020 11775820 <ep11775820@g0.complang.tuwien.ac.at>
2020-12-28 22:51:21 +0100

    perf runs n=16

--- perf_run.sh
+++ perf_run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-perf stat -e cycles -e instructions -e cache-misses -e branch-misses ./nqueens 8 2>perf_result.txt 1>perf_output.txt
+perf stat -e cycles -e instructions -e cache-misses -e branch-misses ./nqueens 16 2>perf_result.txt 1>perf_output.txt
